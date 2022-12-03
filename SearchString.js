52ee9f891a7de81d339856522783791f685ea25c
Jon <32584149+nvpraz0r@users.noreply.github.com>
2022-12-02 20:04:05 -0500

    Update SearchString.js
    added main function to the project

--- SearchString.js
+++ SearchString.js
@@ -8,6 +8,21 @@ function loadBook(filename,displayName){
     document.getElementById("searchstat").innerHTML = "";
     document.getElementById("keyword").innerHTML = "";
 
-    // create request to load txt files
-    
+    // create a server request to load txt file
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.send();
+
+    xhr.onreadystatechange = function () {
+        if(xhr.readyState == 4 && xhr.status == 200){
+            // 
+            currentBook = xhr.responseText;
+            // set content to txt contents
+            document.getElementById("fileContent").innerHTML = currentBook;
+
+            var element = document.getElementById("fileContent");
+            element.scrollTop = 0;
+        }
+    };
+
 }
