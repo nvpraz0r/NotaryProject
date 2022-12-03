b5726e09bffa596dd1e0ae81eedc4d98ffa8dd7c
Jon <32584149+nvpraz0r@users.noreply.github.com>
2022-12-02 19:57:22 -0500

    update
    moved a txt and started logic for search strings

--- SearchString.js
+++ SearchString.js
@@ -1,6 +1,13 @@
 // load a txt file
 function loadBook(filename,displayName){
     let currentBook = "";
-    let url = "";
+    let url = "texts/" + filename;
+
+    // reset book UI before sending in the new book
+    document.getElementById("fileName").innerHTML = displayName;
+    document.getElementById("searchstat").innerHTML = "";
+    document.getElementById("keyword").innerHTML = "";
+
+    // create request to load txt files
     
 }
